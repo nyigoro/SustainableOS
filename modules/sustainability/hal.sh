32cd57b8754228937551b7fb6e6c0436cbdc2448
nyigoro <nyigoro@gmail.com>
2026-01-27 11:59:35 -0800

    commit

--- modules/sustainability/hal.sh
+++ modules/sustainability/hal.sh
@@ -45,11 +45,4 @@ case "$1" in
     eco_cpu_limit) eco_cpu_limit "$2" ;;
     eco_backlight) eco_backlight "$2" ;;
     *) echo "hal.sh {eco_mode_on|eco_mode_off|eco_cpu_limit|eco_backlight}" ;;
-<<<<<<< HEAD
 esac
-
-=======
->>>>>>> 1316ce836c9519eb6063163f43f0c37407ce7746
-=======
-esac
->>>>>>> 0de3f92d58c4c7e752fc2dff351b5e854d2665dc
